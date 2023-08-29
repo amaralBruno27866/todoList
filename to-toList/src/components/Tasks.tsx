ac3431e1abf614ab386b53a2b2373274ee688ad5
amaralBruno27866 <bamaral2.git2023@gmail.com>
2023-08-29 16:04:00 -0400

    some corrections about Tasks component

--- to-toList/src/components/Tasks.tsx
+++ to-toList/src/components/Tasks.tsx
@@ -1,18 +1,24 @@
 import style from '../styles/Tasks.module.css'
-import {Circle, Trash} from 'phosphor-react'
+import {Circle, Trash, CheckCircle} from 'phosphor-react'
+
+interface Task{
+    id: number;
+    content: string;
+    wasCheck: boolean;
+}
 
 export function Tasks(){
     return(
         <div className={style.task}>
-            <span className={style.circle}>
+            <button className={style.circle}>
                 <Circle size={20}/>
-            </span>
+            </button>
             <textarea>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo qui, neque quam, recusandae et voluptates accusantium repellat tenetur velit provident sit id obcaecati iusto quae quibusdam. Alias nisi iste odio?
+                Alias nisi iste odio?
             </textarea>
-            <span className={style.trash}>
-                <Trash size={20} />
-            </span>
+            <button className={style.trash}>
+            <Trash size={20} />
+            </button>
         </div>
     )
 }
