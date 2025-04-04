0a3eaebaf23f1c34b450c793c21119f975e5f441
Bruno27866 <bamaral2.git2023@gmail.com>
2025-04-04 10:58:29 -0400

    PUT: add comments on main.tsx

--- to-toList/src/main.tsx
+++ to-toList/src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './global.css'
-import {App} from './App.tsx'
+import React from 'react' // Importing React library
+import ReactDOM from 'react-dom/client' // Importing ReactDOM for rendering the application
+import './global.css' // Importing global CSS styles
+import { App } from './App.tsx' // Importing the main App component
 
+// Rendering the application
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    {/* HTML: Main application component wrapped in StrictMode for highlighting potential issues */}
     <App />
   </React.StrictMode>,
 )
